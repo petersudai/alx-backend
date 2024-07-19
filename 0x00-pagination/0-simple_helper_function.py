af7aa9361d8b522632289b212d2be2786166794f
petersudai <psudai@gmail.com>
2024-07-19 19:04:19 +0000

    Simple helper function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -4,6 +4,7 @@ function named index_range that takes two integer
 arguments page and page_size
 """
 
+
 def index_range(page, page_size):
     """
     Calculate the start and end index for pagination.
