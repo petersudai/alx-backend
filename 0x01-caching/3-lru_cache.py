5981984b345ee963b0db73eceed219dd07a3b4de
petersudai <psudai@gmail.com>
2024-07-24 17:13:56 +0000

    LRU Caching

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -13,22 +13,22 @@ class LRUCache(BaseCaching):
         super().__init__()
         self.lru_order = []
 
-        def put(self, key, item):
-            """ Add an item in the cache """
-            if key is not None and item is not None:
-                if key in self.cache_data:
-                    self.lru_order.remove(key)
-                elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                    lru_key = self.lru_order.pop(0)
-                    del self.cache_data[lru_key]
-                    print(f"DISCARD: {lru_key}")
-                self.cache_data[key] = item
-                self.lru_order.append(key)
-
-        def get(self, key):
-            """ Get an item by key """
-            if key is None or key not in self.cache_data:
-                return None
-            self.lru_order.remove(key)
+    def put(self, key, item):
+        """ Add an item in the cache """
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                self.lru_order.remove(key)
+            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                lru_key = self.lru_order.pop(0)
+                del self.cache_data[lru_key]
+                print(f"DISCARD: {lru_key}")
+            self.cache_data[key] = item
             self.lru_order.append(key)
-            return self.cache_data[key]
+
+    def get(self, key):
+        """ Get an item by key """
+        if key is None or key not in self.cache_data:
+            return None
+        self.lru_order.remove(key)
+        self.lru_order.append(key)
+        return self.cache_data[key]
