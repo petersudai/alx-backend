bb0ccb14a69dba815593831d04267d38089c1c4e
petersudai <psudai@gmail.com>
2024-07-26 21:06:00 +0000

    Hypermedia pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -47,7 +47,8 @@ class Server:
         assert isinstance(
             page, int) and page > 0, "page must be an integer greater than 0"
         assert isinstance(
-            page_size, int) and page_size > 0, "page_size must be an integer greater than 0"
+            page_size, int) and page_size > 0, \
+            "page_size must be an integer greater than 0"
 
         start_index, end_index = index_range(page, page_size)
         dataset = self.dataset()
